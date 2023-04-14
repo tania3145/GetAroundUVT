cc5f27d35b150334304f0876f125d7d5179f3d1c
Tania Maria <tania.curutchi@gmail.com>
2023-04-14 21:40:28 +0300

    Added bottom toolbar

--- GetAroundUVT/Views/ContentView.swift
+++ GetAroundUVT/Views/ContentView.swift
@@ -16,6 +16,10 @@ struct ContentView: View {
         category: "ContentView"
     )
     
+    init(loadPreview: Bool) {
+        self.loadPreview = loadPreview
+    }
+    
     var body: some View {
         let mapController = MapController(mapRenderer: mapRenderer)
         Task {
@@ -27,29 +31,53 @@ struct ContentView: View {
                 logger.error("ERROR ENCOUNTERED: \(error)")
             }
         }
-        return ZStack {
-            MapComponentView(mapRenderer: $mapRenderer)
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-            VStack {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Button(action: {
-                        mapController.renderer.moveCameraTo(MapRenderer.UVT_LOCATION, withAnimation: true)
-                    }, label: {
-                        Image(systemName: "location.fill")
-                            .imageScale(.large)
-                            .frame(width: 60, height: 60)
-                            .foregroundColor(Color.white)
-                    })
-                    .background(Color.blue)
-                    .cornerRadius(38.5)
-                    .padding()
-                    .shadow(color: Color.black.opacity(0.3),
-                            radius: 3,
-                            x: 3,
-                            y: 3)
-                }
+        return TabView {
+            Group {
+                ZStack {
+                    MapComponentView(mapRenderer: $mapRenderer)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    VStack {
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                                mapController.renderer.moveCameraTo(MapRenderer.UVT_LOCATION, withAnimation: true)
+                            }, label: {
+                                Image(systemName: "location.fill")
+                                    .imageScale(.large)
+                                    .frame(width: 60, height: 60)
+                                    .foregroundColor(Color.white)
+                            })
+                            .background(Color.blue)
+                            .cornerRadius(38.5)
+                            .padding()
+                            .shadow(color: Color.black.opacity(0.3),
+                                    radius: 3,
+                                    x: 3,
+                                    y: 3)
+                        }
+                    }
+                }.background(.white)
+                    .tabItem {
+                        Label("Home", systemImage: "house")
+                    }
+                    .edgesIgnoringSafeArea(.top)
+                
+                VStack {
+                    Text("Friends Screen")
+                }.background(.white)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .tabItem {
+                        Label("Friends", systemImage: "person")
+                    }
+                
+                VStack {
+                    Text("Nearby Screen")
+                }.background(.white)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .tabItem {
+                        Label("Nearby", systemImage: "mappin.circle")
+                    }
             }
         }
     }
